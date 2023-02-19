28316a5947646d9abb4bfb48f51cac1a168af5cc
dvgiap <juuzou152@gmail.com>
2023-02-19 21:24:28 +0700

    updated calculus

--- AdvProg_L2-Calculus/main.cpp
+++ AdvProg_L2-Calculus/main.cpp
@@ -12,18 +12,52 @@ const string PREFIX = "lib_";
 
 double callFunction(string func, double x);
 
+double myCos(double x)
+{
+    double res = cos(x);
+    return res;
+}
+
+/***
+    Args:
+        x (double): a number
+    Returns:
+        double: sine of x
+***/
+double mySin(double x)
+{
+    double res = sin(x);
+    return res;
+}
+
+
+/***
+    Args:
+        x (double): a number
+    Returns:
+        double: square root of x
+***/
+double mySqrt(double x) {
+    if (x < 0) {
+        cout << "Invalid argument" << endl;
+        exit(1);
+    }
+    double res = sqrt(x);
+    return res;
+}
+
 int main(int argc, char* argv[])
 {
     if (argc != 3) {
         cout << "Invalid arguments" << endl;
         return 1;
     }
-
+    std::cin >> argv[1] >> argv[2] >> argv[3];
     string func = argv[1];
     double x = stod(argv[2]);
     cout << "Computed by this program:  " << callFunction(func, x) << endl;
     cout << "Computed by C/C++ library: " << callFunction(PREFIX + func, x) << endl;
-    
+
     return 0;
 }
 
